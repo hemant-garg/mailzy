a16612db2c43286ed539aa974fbbe601aa239d76
Hemant Garg <hmtcool4u@gmail.com>
2018-07-23 22:25:44 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send({
+		developer: 'hemant',
+		age: 20
+	})
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+	console.log(PORT)
+});
