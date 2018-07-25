32f075657fb5542c337d024d93bdb7b2b75704d3
Hemant Garg <hmtcool4u@gmail.com>
2018-07-25 16:53:35 +0530

    finish authentication part

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+require("./models/Users");
+require("./services/passport");
+mongoose.connect(
+	keys.mongoURI,
+	{ useNewUrlParser: true }
+);
 
-app.get('/', (req, res) => {
-	res.send({
-		developer: 'hemant',
-		age: 20,
-		status: 'ready'
+const app = express();
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey]
 	})
-})
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/googleAuth")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-	console.log(PORT)
+	console.log("Server started :", PORT);
 });
