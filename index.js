00828d462a3fb72cb0fefa2f0622c4516f2296bf
Hemant Garg <hmtcool4u@gmail.com>
2018-08-22 19:47:46 +0530

    fixing bu

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const keys = require("./config/keys");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
-const path = require("path");
 require("./models/Users");
 require("./services/passport");
 
@@ -24,14 +23,17 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.static(path.join(__dirname, "client/build")));
-
 require("./routes/googleAuth")(app);
 require("./routes/billingRoutes")(app);
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
