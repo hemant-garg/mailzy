43d2678402356c7faefc190313611eef4e53b8fe
Hemant Garg <hmtcool4u@gmail.com>
2018-08-22 18:27:02 +0530

    fixing bug

--- index.js
+++ index.js
@@ -27,12 +27,11 @@ require("./routes/googleAuth")(app);
 require("./routes/billingRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
-
 	const path = require("path");
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	});
+	app.use(express.static("client/build"));
 }
 
 const PORT = process.env.PORT || 5000;
