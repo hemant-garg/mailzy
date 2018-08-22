5165bb41e79a5b4a45d66a2a7f7d197ff3f42d15
Hemant Garg <hmtcool4u@gmail.com>
2018-08-22 19:35:31 +0530

    fixing

--- index.js
+++ index.js
@@ -20,18 +20,14 @@ app.use(
 		keys: [keys.cookieKey]
 	})
 );
+app.use(express.static("client/build"));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get("/", (req, res) => {
-	res.send("Hey there");
-});
 require("./routes/googleAuth")(app);
 require("./routes/billingRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
-
 	const path = require("path");
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
