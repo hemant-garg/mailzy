f1adbea10a2d7abfa8ae1727496b0e7c03732fae
Hemant Garg <hmtcool4u@gmail.com>
2018-08-23 09:42:55 +0530

    checking

--- index.js
+++ index.js
@@ -30,14 +30,14 @@ app.get("/api/check", (req, res) => {
 	res.send({ working: "true" });
 });
 
-// if (process.env.NODE_ENV === "production") {
-// 	app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
 
-// 	const path = require("path");
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// 	});
-// }
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
