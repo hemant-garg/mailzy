cf9a446cd51e255e2f1d0cff9d520de547bec5b4
Hemant Garg <hmtcool4u@gmail.com>
2018-08-23 09:37:35 +0530

    checking

--- index.js
+++ index.js
@@ -31,8 +31,6 @@ app.get("/api/check", (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
-
 	const path = require("path");
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
