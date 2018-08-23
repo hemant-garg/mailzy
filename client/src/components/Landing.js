db009db1b6fce45d9731353d98b9353109e69dd8
Hemant Garg <hmtcool4u@gmail.com>
2018-08-23 09:34:24 +0530

    checking

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 
 class Landing extends Component {
 	render() {
@@ -7,9 +6,9 @@ class Landing extends Component {
 			<div className="text-center">
 				<h1 className="display-3">Mailzy!</h1>
 				<p className="lead">Collect Feedback from your Users</p>
-				<Link to="/api/check">
+				<a href="/api/check">
 					<button className="btn btn-info">Check</button>
-				</Link>
+				</a>
 			</div>
 		);
 	}
