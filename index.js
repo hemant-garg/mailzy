32801f1bf3911b47bbf609ee70ff80ff394380e1
Hemant Garg <hmtcool4u@gmail.com>
2018-08-23 23:47:25 +0530

    ..

--- index.js
+++ index.js
@@ -27,19 +27,15 @@ app.use(passport.session());
 require("./routes/googleAuth")(app);
 require("./routes/billingRoutes")(app);
 require("./routes/surveyRoutes")(app);
-app.use(express.static(`${__dirname}/client/build`));
-app.get("*", (req, res) => {
-	res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
 
-// if (process.env.NODE_ENV === "production") {
-// 	app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
 
-// 	const path = require("path");
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// 	});
-// }
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
