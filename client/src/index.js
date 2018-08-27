cc76c515199fd82996cd2ac36713ac0df25080e2
Hemant Garg <hmtcool4u@gmail.com>
2018-08-27 18:36:06 +0530

    done with all functionality

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,8 @@ import registerServiceWorker from "./registerServiceWorker";
 import reducers from "./reducers";
 import App from "./components/App";
 
+import axios from "axios";
+window.axios = axios;
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
