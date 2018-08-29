904e6f975003e8815f1891f0b19899793d314efe
Hemant Garg <hmtcool4u@gmail.com>
2018-08-29 19:25:47 +0530

    index.js updated

--- index.js
+++ index.js
@@ -30,11 +30,11 @@ require("./routes/billingRoutes")(app);
 require("./routes/surveyRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
+	app.use(express.static(path.join(__dirname, "client/build")));
 
 	const path = require("path");
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+		res.sendFile(path.join(__dirname + "/client/build/index.html"));
 	});
 }
 
